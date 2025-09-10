569cb0c2bb3bb2da570202e0e9890b7a45021c87
FaisalIbrahim404 <ff9371668@gmail.com>
2025-09-10 14:39:29 +0300

    [enhace] Enhance Navbar

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,6 @@
+/// <reference types="astro/client" />
+/// <reference types="astro/astro-jsx" />
+/// <reference path="../.astro/types.d.ts" />
+
 // Custom types declarations
 declare let myString: string;
